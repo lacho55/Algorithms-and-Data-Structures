10041d309ed0a6c5c12ad3f096b8cdd5e977d35d
Lachezar Peshterliev <lacho_fitness@abv.bg>
2020-10-17 16:09:42 +0300

    main data and methods

--- Data Structures/LinkedLists/DLList/DLList.h
+++ Data Structures/LinkedLists/DLList/DLList.h
@@ -1,1 +1,26 @@
+#pragma once
 
+template<class T>
+struct Node{
+    T data;
+    Node<T>* next;
+    Node<T>* prev;
+    Node(const T&, Node<T>* = nullptr, Node<T>* = nullptr);
+};
+
+template<class T>
+class DLList{
+    Node<T>* first;
+    Node<T>* last;
+    size_t currentSize;
+
+    /*---- Helper Functions ----*/
+    void copy(const DLList<T>&);
+    void destroy();
+
+    public:
+        DLList();
+        DLList(const DLList<T>&);
+        DLList<T>& operator=(const DLList<T>&);
+        ~DLList();
+};
