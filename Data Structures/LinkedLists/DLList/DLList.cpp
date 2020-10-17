08c1470d9ef9e2ac60079f0f9a8c91f4982a791f
Lachezar Peshterliev <lacho_fitness@abv.bg>
2020-10-18 00:00:46 +0300

    adding aditional functions

--- Data Structures/LinkedLists/DLList/DLList.cpp
+++ Data Structures/LinkedLists/DLList/DLList.cpp
@@ -1,4 +1,4 @@
-
+#include <assert.h>
 #include "DLList.h"
 
 //Node constructor
@@ -79,4 +79,133 @@ DLList<T>& DLList<T> :: operator = (const DLList<T>& other){
 template<class T>
 DLList<T> :: ~DLList(){
     destroy();
+}
+
+
+template<class T>
+void DLList<T> :: push_back(const T& data){
+    Node<T>* toAdd = new Node<T>(data, nullptr, last);
+
+    if(!first){
+        first = last = toAdd;
+
+    }else{
+        last->next = toAdd;
+        last = last->next;
+    }
+    currentSize++;
+}
+
+
+template<class T>
+void DLList<T> :: push_front(const T& data){
+    Node<T>* toAdd = new Node<T>(data,first, nullptr); // we can skipp defining nullptr for the last pointer
+
+    if(first){
+        first->prev = toAdd;
+    }
+    first = toAdd;
+    if(!last){
+        first = last;
+    }
+    currentSize++;
+
+}
+
+
+template<class T>
+void DLList<T> :: pop_back(){
+    if(first){
+        if(first == last){
+            delete first;
+            first = last = nullptr;
+            return;
+        }
+
+        Node<T>* toDelete = last;
+        last = last->prev;
+        last->next = nullptr;
+        delete toDelete;
+        currentSize--;
+
+    }
+}
+
+
+template<class T>
+void DLList<T> :: pop_front(){
+    if(first){
+        if(first == last){
+            delete first;
+            first = last = nullptr;
+            return;
+        }
+        Node<T>* toDelete = first;
+        first = first->next;
+        first->prev = nullptr;
+        delete toDelete;
+        currentSize--;
+
+    }
+}
+
+
+template<class T>
+T DLList<T> :: front() const{
+    assert(first);
+    return first->data;
+}
+
+
+template<class T>
+T DLList<T> :: back() const{
+    assert(last);
+    return last->data;
+}
+
+
+template<class T>
+bool DLList<T> :: empty() const{
+    return first == nullptr;
+}
+
+
+template<class T>
+size_t DLList<T> :: size() const{
+    return currentSize;
+}
+
+
+template<class T>
+void DLList<T> :: print() const{
+
+    Node<T>* curr = first;
+
+    std :: cout << "{ ";
+
+    for(size_t i = 0; i < currentSize - 1; ++i){
+        std :: cout << curr->data << ", ";
+        curr = curr->next;
+    }
+    std :: cout << curr->data << " }\n";
+}
+
+
+template<class T>
+void DLList<T> :: reverse() {
+    if((first == nullptr) || (currentSize == 1)){
+        return;
+    }
+
+    Node<T>* current = first;
+    Node<T>* temp = last;
+    last = first;
+    first = temp;
+
+    while(current != nullptr){
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        current = current->prev;
+    }
 }
