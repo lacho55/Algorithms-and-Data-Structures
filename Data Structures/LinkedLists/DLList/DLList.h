c612b9da48fec46e8f860afd8347ef9370d3c561
Lachezar Peshterliev <lacho_fitness@abv.bg>
2020-10-23 19:11:12 +0300

    Update DLList.h

--- Data Structures/LinkedLists/DLList/DLList.h
+++ Data Structures/LinkedLists/DLList/DLList.h
@@ -5,24 +5,31 @@ struct Node{
     T data;
     Node<T>* next;
     Node<T>* prev;
+
+    /* ---- Node constructor ---- */
     Node(const T&, Node<T>* = nullptr, Node<T>* = nullptr);
 };
 
+
 template<class T>
 class DLList{
+    private:
     Node<T>* first;
     Node<T>* last;
     size_t currentSize;
 
-    /*---- Helper Functions ----*/
+    /* ----- Helper Functions ---- */
     void copy(const DLList<T>&);
     void destroy();
 
+    /* ---- DLList Constructors ---- */
     public:
         DLList();
         DLList(const DLList<T>&);
         DLList<T>& operator=(const DLList<T>&);
         ~DLList();
+
+        /* ---- Main Functions ---- */
         void push_back(const T& data);
         void push_front(const T& data);
         void pop_back();
@@ -31,6 +38,6 @@ class DLList{
         T back() const;
         bool empty() const;
         size_t size() const;
-        void print() const;
+        void print();
         void reverse();
 };
